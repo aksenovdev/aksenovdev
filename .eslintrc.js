42ecb77b15b61259553da6f7074517ba761c72f0
Aksenov Sergei <aksenovdev@gmail.com>
2018-06-18 02:18:01 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential'],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'curly': 'error',
+    'eqeqeq': 'error',
+    'no-magic-numbers': 'error',
+    'radix': 'error',
+    'yoda': 'error',
+    'indent': ['error', 2],
+    'camelcase': 'error',
+    'linebreak-style': ['error', 'windows'],
+    'quotes': ['error', 'single', {'avoidEscape': true}],
+    'semi': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'no-console': 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    router-view
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.test-one {
+  .test-two {
+    background: black;
+  }
+}
+</style>
